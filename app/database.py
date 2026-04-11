444948cd88e4f4a1b7d37bff1cdd48b4d65a9532
atul <atul@atuls-MacBook-Air.local>
2026-04-11 14:49:31 +0530

    fetching data

--- app/database.py
+++ app/database.py
@@ -13,17 +13,25 @@ cursor.execute("""
     )
 """)
 
+# cursor.execute("""
+#     INSERT INTO shipment
+#     VALUES
+#         (12701, "glassware", 5.6, "placed"),
+#         (12702, "electronics", 1.5, "in_transit"),
+#         (12703, "furniture", 24.5, "delivered"),
+#         (12704, "documents", 1.2, "placed"),
+#         (12705, "kitchenware", 12.8, "in_transit"),
+#         (12706, "clothing", 2.2, "out_for_delivery"),
+#         (12707, "books", 3.5, "delivered")
+# """)
+
 cursor.execute("""
-    INSERT INTO shipment
-    VALUES
-        (12701, "glassware", 5.6, "placed"),
-        (12702, "electronics", 1.5, "in_transit"),
-        (12703, "furniture", 24.5, "delivered"),
-        (12704, "documents", 1.2, "placed"),
-        (12705, "kitchenware", 12.8, "in_transit"),
-        (12706, "clothing", 2.2, "out_for_delivery"),
-        (12707, "books", 3.5, "delivered")
+    SELECT *
+    FROM shipment
+    WHERE id = 12701
 """)
+
+print(cursor.fetchone())
 
 
 connection.commit()
