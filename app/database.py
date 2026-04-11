690cb53df85572b31be6748477401ac65b0228b2
atul <atul@atuls-MacBook-Air.local>
2026-04-11 14:45:42 +0530

    added sqlite db

---
+++ app/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+connection = sqlite3.connect("sqlite.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS shipment (
+        id INTEGER,
+        content TEXT,
+        weight REAL,
+        status TEXT
+    )
+""")
+
+
+connection.close()
